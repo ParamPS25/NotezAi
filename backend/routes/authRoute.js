6559dd8cc05d794a88f0a50b2167f62646e2a828
ParamPS25 <bhavsarparam1940@gmail.com>
2025-05-16 04:09:25 +0530

    Revert "deploy changes 2"
    This reverts commit ea24566fedcaa4b21fede7da0eacae3c1618487a.

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -8,8 +8,8 @@ router.get('/google',passport.authenticate('google',{scope : ['profile','email']
 
 router.get('/google/callback',
     passport.authenticate('google', {
-        successRedirect : `${process.env.CLIENT_URL}/auth/success`,
-        failureRedirect : `${process.env.CLIENT_URL}/auth/failure`,
+        successRedirect : `${process.env.CLIENT_URL}`,
+        failureRedirect : `${process.env.CLIENT_URL}`
     }),
 );
 
